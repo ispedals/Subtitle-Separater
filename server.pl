a69283f1b7a3fe3c7f24cc67eb4433a3fc86f7ec
Balloonguy <balloonguy123@gmail.com>
2013-01-13 17:54:10 -0500

    properly connect controller with video

--- server.pl
+++ server.pl
@@ -4,12 +4,25 @@ use warnings;
 
 use Mojolicious::Lite;
 
+my $send_to_controller;
+my $send_to_video;
+
 # WebSocket echo service
-websocket '/echo' => sub {
-	my $self = shift;
-	$self->on(message => sub {
+websocket '/messageToController' => sub {
+	$send_to_video = shift;
+	$send_to_video->on(message => sub {
 		my ($self, $msg) = @_;
 		$self->send("echo: $msg");
+		eval { $send_to_controller->send("echo: $msg");};
+	});
+};
+
+
+websocket '/messageToVideo' => sub {
+	$send_to_controller = shift;
+	 $send_to_controller->on(message => sub {
+		my ($self, $msg) = @_;
+		eval {$send_to_video->send("echo: $msg");};
 	});
 };
 
