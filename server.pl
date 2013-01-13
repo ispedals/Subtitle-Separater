040fe61227bf3bdc5e381b0911efdd544f34fa3a
Balloonguy <balloonguy123@gmail.com>
2013-01-13 18:49:48 -0500

    start ability to display subtitles based on time; and only send the duration to the controller

--- server.pl
+++ server.pl
@@ -7,22 +7,19 @@ use Mojolicious::Lite;
 my $send_to_controller;
 my $send_to_video;
 
-# WebSocket echo service
 websocket '/messageToController' => sub {
 	$send_to_video = shift;
 	$send_to_video->on(message => sub {
 		my ($self, $msg) = @_;
-		$self->send("echo: $msg");
-		eval { $send_to_controller->send("echo: $msg");};
+		eval { $send_to_controller->send("$msg");};
 	});
 };
 
-
 websocket '/messageToVideo' => sub {
 	$send_to_controller = shift;
-	 $send_to_controller->on(message => sub {
+	$send_to_controller->on(message => sub {
 		my ($self, $msg) = @_;
-		eval {$send_to_video->send("echo: $msg");};
+		eval {$send_to_video->send($msg);};
 	});
 };
 
