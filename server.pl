e61dc68e74e6a33057e5d5d105daf0d02d69a16e
Balloonguy <balloonguy123@gmail.com>
2013-01-14 19:12:32 -0500

    increase websocekt timeout

--- server.pl
+++ server.pl
@@ -3,12 +3,14 @@ use v5.16;
 use warnings;
 
 use Mojolicious::Lite;
+use Mojo::IOLoop;
 
 my $send_to_controller;
 my $send_to_video;
 
 websocket '/messageToController' => sub {
 	$send_to_video = shift;
+	Mojo::IOLoop->stream($send_to_video->tx->connection)->timeout(300);
 	$send_to_video->on(message => sub {
 		my ($self, $msg) = @_;
 		eval { $send_to_controller->send("$msg");};
@@ -17,6 +19,7 @@ websocket '/messageToController' => sub {
 
 websocket '/messageToVideo' => sub {
 	$send_to_controller = shift;
+	Mojo::IOLoop->stream($send_to_controller->tx->connection)->timeout(300);
 	$send_to_controller->on(message => sub {
 		my ($self, $msg) = @_;
 		eval {$send_to_video->send($msg);};
